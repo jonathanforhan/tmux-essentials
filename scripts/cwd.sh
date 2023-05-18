b2d3daae3c6ca66a9b0ade7a0028b16da03b585f
jonathanforhan <jonathan.forhan@gmail.com>
2023-05-17 20:42:39 -0400

    initial commmit

---
+++ scripts/cwd.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# return current working directory of tmux pane
+getPaneDir()
+{
+    nextone="false"
+    ret=""
+    for i in $(tmux list-panes -F "#{pane_active} #{pane_current_path}");
+    do
+        [ "$i" == "1" ] && nextone="true" && continue
+        [ "$i" == "0" ] && nextone="false"
+        [ "$nextone" == "true" ] && ret+="$i "
+    done
+    echo "${ret%?}"
+}
+# two possible locations of plugin
+source "$HOME/.config/tmux/plugins/tmux/data/nvim_buffer"
+source "$HOME/.tmux/plugins/tmux/data/nvim_buffer"
+
+path="$nvim_buffer"
+[ -z "$path" ] && path=$(getPaneDir)
+
+# change '/home/user' to '~'
+echo "${path/"$HOME"/'~'}"
